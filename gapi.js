97ddefda6e60f8af152780b930a33e5e66dd565c
msamardar <mehransamardar@mehrans-mbp-2.attlocal.net>
2017-10-10 21:55:51 -0700

    Refactor to simplify

--- gapi.js
+++ gapi.js
@@ -1,9 +1,11 @@
 module.exports = {
-    CLIENT_ID: "1048668753326-l7u88gem4bo4val0o6nfa4kv5ai6h8h0",
+    // CLIENT_ID: "1048668753326-l7u88gem4bo4val0o6nfa4kv5ai6h8h0",
+    CLIENT_ID: "840805447866-abl7ch84i5d1fgt4nsg785m0t809i9bl",
     APP_ID: "1048668753326",
-    CLIENT_SECRET:"FfACnRAmPHqUElbBSAgAi9Gw",
-    // REDIRECT_URL:"http://localhost:3000",
-    REDIRECT_URL:"https://datagate.herokuapp.com/",
+    //CLIENT_SECRET:"FfACnRAmPHqUElbBSAgAi9Gw",
+    CLIENT_SECRET:"Ipm0YgswYJc905M7DCj1HYRt",
+    REDIRECT_URL:"http://localhost:3000",
+    //REDIRECT_URL:"https://datagate.herokuapp.com/",
     SENDGRID_API_KEY:"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",
     EmailID: "YOUR_EMAIL@gmail.com"
 }
