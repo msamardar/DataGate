d35496f97c7d0561642f6ff9b0e290833b3fea22
msamardar <mehransamardar@mehrans-mbp-2.attlocal.net>
2017-10-10 18:56:25 -0700

    4th

--- gapi.js
+++ gapi.js
@@ -2,7 +2,8 @@ module.exports = {
     CLIENT_ID: "1048668753326-l7u88gem4bo4val0o6nfa4kv5ai6h8h0",
     APP_ID: "1048668753326",
     CLIENT_SECRET:"FfACnRAmPHqUElbBSAgAi9Gw",
-    REDIRECT_URL:"http://localhost:3000",
+    // REDIRECT_URL:"http://localhost:3000",
+    REDIRECT_URL:"https://datagate.herokuapp.com/",
     SENDGRID_API_KEY:"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",
     EmailID: "YOUR_EMAIL@gmail.com"
 }
